f2bee13a78813951aa8acb5164ec1723a7bbcdff
Lin Hsin-An <hsinan.lin817@gmail.com>
2023-11-09 16:15:21 +0800

    try json pipeline

--- scraptt/settings.py
+++ scraptt/settings.py
@@ -71,8 +71,8 @@ EXTENSIONS = {
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 ITEM_PIPELINES = {
-    "scraptt.pipelines.CkipPipeline": 300,
-    # "scraptt.pipelines.JsonPipeline": 300,
+    #"scraptt.pipelines.CkipPipeline": 300,
+    "scraptt.pipelines.JsonPipeline": 300,
 }
 
 # Enable and configure the AutoThrottle extension (disabled by default)
